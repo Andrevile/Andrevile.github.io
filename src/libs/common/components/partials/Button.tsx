4ddeb80b01aff893a0cbfa1c0e1b6bada8a6f4e4
abg3000 <abg3000@naver.com>
2023-10-24 20:26:42 +0900

    feat: 다크모드 구현

---
+++ src/libs/common/components/partials/Button.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface ButtonProps extends HTMLButtonElement {}
+
+const Button = () => {
+  return <button></button>;
+};
+
+export default Button;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css';
+import 'dayjs/locale/ko';
+
+import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { createContext, useContext } from 'react';
+
+import { useDarkMode } from '@/libs/common/hooks/useDarkMode';
+import { font } from '@/libs/common/styles/font';
+import { globalStyle } from '@/libs/common/styles/global';
+import { reset } from '@/libs/common/styles/reset';
+import { theme } from '@/libs/common/styles/theme';
+
+const ThemeProviderContext = createContext({
+  mode: 'light',
+  toggleMode: () => {},
+});
+
+export const useThemeContext = () => useContext(ThemeProviderContext);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { mode, toggleMode } = useDarkMode();
+
+  return (
+    <ThemeProviderContext.Provider value={{ mode, toggleMode }}>
+      <ThemeProvider theme={theme(mode)}>
+        <Global styles={font} />
+        <Global styles={reset} />
+        <Global styles={globalStyle} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ThemeProviderContext.Provider>
+  );
 }
