34e3e4e467905849bcbe7a6e111e7be37d304675
abg3000 <abg3000@naver.com>
2023-10-27 00:21:45 +0900

    feat: tailwind css 다크모드 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,11 @@ import '@/styles/global.css';
 import 'dayjs/locale/ko';
 
 import type { AppProps } from 'next/app';
-import { createContext, useContext } from 'react';
-
-const ThemeProviderContext = createContext({
-  mode: 'light',
-  toggleMode: () => {},
-});
-
-export const useThemeContext = () => useContext(ThemeProviderContext);
-
+import { ThemeProvider } from 'next-themes';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,10 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-import DarkModeScript from '@/libs/common/components/html/script/DarkModeScript';
-
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
-      <DarkModeScript />
+
       <body>
         <Main />
         <NextScript />
