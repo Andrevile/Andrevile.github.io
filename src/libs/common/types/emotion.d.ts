36dfeb1fcc21579c408c9602f5f30fe20cbf7d4d
abg3000 <abg3000@naver.com>
2023-10-26 23:13:42 +0900

    feat: tailwind 로 전환

--- src/libs/common/types/emotion.d.ts
+++
@@ -1,7 +0,0 @@
-import '@emotion/react';
-
-import { Theme as DefaultThemeType } from './types';
-
-declare module '@emotion/react' {
-  export interface Theme extends DefaultThemeType {}
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,16 +12,5 @@ const ThemeProviderContext = createContext({
 export const useThemeContext = () => useContext(ThemeProviderContext);
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { mode, toggleMode } = useDarkMode();
-
-  return (
-    <ThemeProviderContext.Provider value={{ mode, toggleMode }}>
-      <ThemeProvider theme={theme(mode)}>
-        <Global styles={font} />
-        <Global styles={reset} />
-        <Global styles={globalStyle} />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ThemeProviderContext.Provider>
-  );
+  return <Component {...pageProps} />;
 }
