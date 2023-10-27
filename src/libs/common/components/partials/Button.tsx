73e9964cf915d3194b28a6bf73358dd186fbea6b
abg3000 <abg3000@naver.com>
2023-10-28 01:29:30 +0900

    chore: favicon, 네비게이션 아이콘 적용

--- src/libs/common/components/partials/Button.tsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface ButtonProps extends HTMLButtonElement {}
-
-const Button = () => {
-  return <button></button>;
-};
-
-export default Button;
